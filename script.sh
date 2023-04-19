1825213c747d43da77f1c278f41bf69f0b046cb7
Franciszek 51012 <dsw51012@student.dsw.edu.pl>
2023-04-19 20:56:12 +0200

    added error, modified .gitignore, finished --help

--- script.sh
+++ script.sh
@@ -31,6 +31,7 @@ case $1 in
 	"--help"|"-h")
 		echo "--date, -d - aktualna data i godzina"
 		echo "--logs, -l - log, mozna wybrac ilosc zapisywanych logow dopisujac liczbe, bazowo jest ich 100"
+		echo "--error, -e - to samo co log"
 		echo "--help, -h - wyswietla kazda komende"
 		echo "--init - klonuje repo do katalogu w ktorym zostal uruchomiony skrypt"
 	;;
@@ -38,7 +39,31 @@ case $1 in
 		git clone https://github.com/xKitsune47/narzedziapracywbranzy_lab3
 		newPath=$(pwd)
 		echo "export PATH=\$PATH:$newPath">>~/.bashrc
+	;;
+	"--error")
+		if [ $number -gt 0 ]
+		then
+			for err in $(seq 1 $2)
+			do
+				fileName="error${err}"
+				touch $fileName.txt
+				echo "error${err}">>$fileName.txt
+				echo $0>>$fileName.txt
+				echo $today>>$fileName.txt
+			done
+		else
+			for err in {0..100};
+			do
+				fileName="error${err}"
+				touch $fileName.txt
+				echo "error${err}">>$fileName.txt
+				echo $0>>$fileName.txt
+				echo $today>>$fileName.txt
+			done
+		fi
+	;;
 esac
 
 touch .gitignore
-echo "*log*">.gitignore
+echo "*log*">>.gitignore
+echo "*error*">>.gitignore
