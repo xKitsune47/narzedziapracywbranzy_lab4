b413f86d234a3e726d2da5892e1cbb488fc23d2c
Franciszek 51012 <dsw51012@student.dsw.edu.pl>
2023-04-19 19:42:13 +0200

    added logs and .gitignore

--- script.sh
+++ script.sh
@@ -6,5 +6,28 @@ today=$(date)
 #wybierz operacje
 case $1 in
 	"--date")echo "$today";;
-	"--logs");;
+	"--logs")
+		if [ $number -gt 0 ]
+		then
+			for logs in $(seq 1 $2)
+			do
+				fileName="log${logs}"
+				touch $fileName.txt
+				echo "log${logs}">>$fileName.txt
+				echo $0>>$fileName.txt
+				echo $today>>$fileName.txt
+			done
+		else
+			for logs in {0..100};
+			do
+				fileName="log${logs}"
+				touch $fileName.txt
+				echo "log${logs}">>$fileName.txt
+				echo $0>>$fileName.txt
+				echo $today>>$fileName.txt
+			done
+		fi;;
 esac
+
+touch .gitignore
+echo "*log*">.gitignore
